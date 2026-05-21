a14a7d87474dc24107a1788f1b81d6250d76ecea
111202416073-collab <111202416073@mhs.dinus.ac.id>
2026-05-21 20:22:26 +0700

    Routes.php

--- kuliner-app/app/Config/Routes.php
+++ kuliner-app/app/Config/Routes.php
@@ -7,46 +7,87 @@ use CodeIgniter\Router\RouteCollection;
  */
 
 $routes->get('/', 'Auth::login');
+
 $routes->get('login', 'Auth::login');
 $routes->post('auth/prosesLogin', 'Auth::prosesLogin');
 $routes->get('auth/logout', 'Auth::logout');
 
-$routes->get('kuliner', 'Kuliner::index');
-$routes->get('kuliner/create', 'Kuliner::create');
-$routes->post('kuliner/save', 'Kuliner::save');
-$routes->get('kuliner/edit/(:num)', 'Kuliner::edit/$1');
-$routes->post('kuliner/update/(:num)', 'Kuliner::update/$1');
-$routes->get('kuliner/delete/(:num)', 'Kuliner::delete/$1');
-
 $routes->get('auth/login', 'Auth::login');
 $routes->post('auth/prosesLogin', 'Auth::prosesLogin');
 $routes->get('auth/logout', 'Auth::logout');
+
 $routes->get('pengunjung', 'Auth::pengunjung');
 
+// ROUTE KULINER
+
+$routes->get('kuliner', 'Kuliner::index');
+$routes->get('kuliner/create', 'Kuliner::create');
+$routes->post('kuliner/save', 'Kuliner::save');
+
+$routes->get('kuliner/edit/(:num)', 'Kuliner::edit/$1');
+$routes->post('kuliner/update/(:num)', 'Kuliner::update/$1');
+
+$routes->get('kuliner/delete/(:num)', 'Kuliner::delete/$1');
+
+$routes->get('kuliner/detail/(:num)', 'Kuliner::detail/$1');
+
+
+// ROUTE KATEGORI
+
 $routes->get('kategori', 'Kategori::index');
+
 $routes->get('kategori/create', 'Kategori::create');
 $routes->post('kategori/save', 'Kategori::save');
+
+$routes->get('kategori/edit/(:num)', 'Kategori::edit/$1');
+$routes->post('kategori/update/(:num)', 'Kategori::update/$1');
+
+$routes->get('kategori/delete/(:num)', 'Kategori::delete/$1');
+
+
+// ROUTE PROFIL
 
 $routes->get('profil', 'Profil::index');
 
 
+// ROUTE STATISTIK
+
 $routes->get('laporan', 'Statistik::index');
 $routes->get('statistik', 'Statistik::index');
 
+// ROUTE REVIEW
 
-$routes->get('kuliner/detail/(:num)', 'Kuliner::detail/$1');
 $routes->post('review/save', 'Review::save');
 
 
+// ROUTE FILTER AUTH
+
 $routes->group('', ['filter' => 'auth'], function ($routes) {
+
+    // Kuliner
     $routes->get('kuliner/create', 'Kuliner::create');
     $routes->post('kuliner/save', 'Kuliner::save');
+
     $routes->get('kuliner/edit/(:num)', 'Kuliner::edit/$1');
     $routes->post('kuliner/update/(:num)', 'Kuliner::update/$1');
-    $routes->delete('kuliner/(:num)', 'Kuliner::delete/$1');
 
+    $routes->get('kuliner/delete/(:num)', 'Kuliner::delete/$1');
+
+
+    // Profil
     $routes->get('profil', 'Profil::index');
 
+
+    // Jelajah
     $routes->get('jelajah', 'Jelajah::index');
     $routes->get('jelajah/detail/(:num)', 'Jelajah::detail/$1');
+
+
+    // API
+    $routes->get('api/kuliner', 'Api\KulinerApi::index');
+
+
+    // Payment
+    $routes->get('payment/beli/(:num)', 'Payment::beliVoucher/$1');
+    $routes->post('payment/notification', 'Payment::notification');
 });
